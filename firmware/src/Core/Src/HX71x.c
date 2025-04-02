4a1de271e18ffbfe30027135d779517e85974b2e
iforce2d <iforce2d@gmail.com>
2025-04-03 07:27:11 +1300

    fixed signedness of load cell values, also pull load cell clock high on startup as mentioned in datasheet

--- firmware/src/Core/Src/HX71x.c
+++ firmware/src/Core/Src/HX71x.c
@@ -12,6 +12,11 @@
 
 void initHX71x() {
 
+	// from datasheet: pull SCK high for more than 100us and then pull it low at startup
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
+	HAL_Delay( 5 ); // milliseconds
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+
 	MX_SPI1_Init_hx71x();
 }
 
@@ -39,8 +44,14 @@ void updateHX71x() {
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi)
 {
 	if ( hspi->Instance == SPI1 ) {
-		int32_t tmpInt = ((uint32_t) hx71x_data[0] << 16) | ((uint32_t) hx71x_data[1] << 8) | (uint32_t) hx71x_data[2];
-		tmpInt -= 0x800000;
+
+		int32_t tmpInt = (((int32_t) hx71x_data[0]) << 16) | (((int32_t) hx71x_data[1]) << 8) | ((int32_t) hx71x_data[2]);
+
+		// Seems like the sign bit (hx71x_data[0] & 0x80) never goes high, no matter what the value!
+		// Using the next bit seems to work, but still not completely sure this is right...
+		if ( hx71x_data[0] & 0x40 )
+			tmpInt -= 0x800000;
+
 		txData.loadcell = tmpInt;
 	}
 }
